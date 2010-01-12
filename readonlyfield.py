dbfcfa678228fd63145e3eb13bfa5c9fa704d9a3
Lakshman Prasad <lakshman@uswaretech.com>
2010-01-12 18:58:52 +0530

    new read nly widget

--- readonlyfield.py
+++ readonlyfield.py
@@ -4,6 +4,7 @@ from django.forms.util import flatatt
 from django import forms
 
 class ReadOnlyWidget(forms.Widget):
+    
     def render(self, name, value, attrs):
         final_attrs = self.build_attrs(attrs, name=name)
         if hasattr(self, 'initial'):
@@ -22,3 +23,21 @@ class ReadOnlyField(forms.Field):
 
     def clean(self, value):
         return self.widget.initial
+    
+class ReadOnlyWidget2(forms.Widget):
+
+    def __init__(self, original_value, display_value):
+        self.original_value = original_value
+        self.display_value = display_value
+        super(ReadOnlyWidget2, self).__init__()
+        
+    def _has_changed(self, initial, data):
+        return False
+        
+    def render(self, name, value, attrs=None):
+        if self.display_value is not None:
+            return unicode(self.display_value)
+        return unicode(self.original_value)
+
+    def value_from_datadict(self, data, files, name):
+        return self.original_value
